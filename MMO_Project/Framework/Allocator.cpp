1c17bbbcd0e0461fc65fbf1aa6f227634df2ab26
DongWookK <dev.Dongwook@gmail.com>
2022-04-17 21:36:35 +0900

    Allocator Start

---
+++ MMO_Project/Framework/Allocator.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include "Allocator.h"
+
+
+void* Allocator::Alloc(int32 size)
+{
+	return ::malloc(size);
+}
+
+void Allocator::Release(void* ptr)
+{
+	::free(ptr);
+}
---
+++ MMO_Project/Framework/Allocator.h
@@ -0,0 +1,8 @@
+#pragma once
+class Allocator
+{
+public:
+	static void*	Alloc(int32 size);
+	static void		Release(void* ptr);
+};
+
