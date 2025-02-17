e25694883451834bffd75f679521e88f7f660058
DongWook Kim <49301812+DongWookK@users.noreply.github.com>
2025-02-17 22:00:12 +0900

    server acceptor

--- MMO_Project/Client/main.cpp
+++ MMO_Project/Client/main.cpp
@@ -5,6 +5,7 @@ using namespace boost;
 
 int main()
 {
+#pragma region get_endpoint
 	// point Client to Server's endpoint
 	std::string raw_ip_address = "127.0.0.1";
 	unsigned short port_num = 3333;
@@ -19,6 +20,34 @@ int main()
 	}
 
 	asio::ip::tcp::endpoint ep(ip_address, port_num);
+#pragma endregion
+	
+#pragma region create active socket
 
+	// boost.asio의 핵심
+	// 이 클래스의 객체를 통해 운영체제가 제공하는 네트워크 I/O 서비스로 접근할 수 있다.
+	asio::io_context ioc;
+
+	// 프로토콜을 표현하는 값들을 갖는 데이터 구조체역할. 기능은 제공하지 않는다.
+	asio::ip::tcp protocol = asio::ip::tcp::v4();
+
+	// instantiate active tcp socket
+	asio::ip::tcp::socket sock(ioc);
+	sock.open(protocol, ec); // 실제 운영체제 소켓이 할당된다.
+
+	if (ec.value() != 0)
+	{
+		std::cout << "Failed to open socket. Error Code = " << ec.value() << ". Message:: " << ec.message();
+		return ec.value();
+	}
+#pragma endregion
+
+	
+
+	
+	
 	return 0;
+
+
+
 }
--- MMO_Project/Framework/acceptor.h
+++ MMO_Project/Framework/acceptor.h
@@ -3,6 +3,25 @@
 
 using namespace boost;
 
+auto get_acceptor() -> std::shared_ptr<asio::ip::tcp::acceptor>
+{
+	boost::system::error_code ec;
+
+	asio::io_context ioc; // todo - conver to in-parameter variable
+
+	asio::ip::tcp protocol = asio::ip::tcp::v4();
+	auto acceptor = std::make_shared<asio::ip::tcp::acceptor>(ioc);
+	acceptor->open(protocol, ec); // in this state, dosen't listen.
+	
+	if (0 != ec.value())
+	{
+		std::cout << "failed to open the acceptor socket Error Code = " << ec.value();
+	}
+
+	return acceptor;
+}
+
+
 auto get_endpoint() -> const asio::ip::tcp::endpoint&
 {
 	static unsigned short port_num = 0221;
