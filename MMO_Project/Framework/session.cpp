93072112398ad8bb515d89a7e1968ee3535b02ee
DongWook Kim <49301812+DongWookK@users.noreply.github.com>
2026-07-29 16:46:13 +0900

    fix: session async read

--- MMO_Project/Framework/session.cpp
+++ MMO_Project/Framework/session.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "pch.h"
 #include "session.h"
 #include "boost/asio.hpp"
@@ -14,8 +15,9 @@ auto session::get_index() const -> size_t
 
 auto session::on_accept() -> void
 {
-	std::cout << "client connected " << std::endl;
-	read_from_socket();
+	std::cout << "Client connected (Session index: " << index_ << ")" << std::endl;
+
+	read_from_socket(); // 비동기 수신 루프 시작
 }
 
 auto session::reset() -> void
@@ -27,10 +29,37 @@ auto session::reset() -> void
 	}
 }
 
-auto session::read_from_socket() -> std::string
+auto session::read_from_socket() -> void
 {
-	asio::read(socket_, asio::buffer(buf_, message_size));
-	return std::string(buf_.data(), message_size);
+	socket_.async_read_some(
+		asio::buffer(buf_),
+		[this, self = shared_from_this()](const boost::system::error_code& error, std::size_t bytes_transferred) // 수신 중 파괴 방지를 위한 shared_from_this
+		{
+			if (!error)
+			{
+				std::string received_msg(buf_.data(), bytes_transferred);
+				std::cout << "[Session " << index_ << "] Recv (" << bytes_transferred << " bytes): " << received_msg << std::endl;
+
+				// TODO: 여기서 수신된 데이터를 패킷 단위로 조립하거나 핸들러로 전달
+
+				read_from_socket();
+			}
+			else
+			{
+				if (error == asio::error::eof)
+				{
+					std::cout << "Client disconnected cleanly (Session index: " << index_ << ")" << std::endl;
+					// TODO: 세션 삭제, 풀 반납 필요
+				}
+				else
+				{
+					std::cout << "Read fail. Error code: " << error.value() << ", Msg: " << error.message() << std::endl;
+				}
+
+				reset();
+			}
+		}
+	);
 }
 
 auto session::get_socket() -> tcp_t::socket&
--- MMO_Project/Framework/session.h
+++ MMO_Project/Framework/session.h
@@ -5,30 +5,29 @@
 using namespace boost;
 using tcp_t = boost::asio::ip::tcp;
 
+namespace asio = boost::asio;
+using tcp_t = asio::ip::tcp;
+
+// std::enable_shared_from_this를 상속받아 비동기 람다 안에서 세션 파괴 방지
 class session : public std::enable_shared_from_this<session>
 {
-    static constexpr size_t message_size = 10;
-
 public:
-    explicit session(boost::asio::io_context& worker_context)
-        : socket_(worker_context)
-        , index_(0)
-    {}
+	explicit session(asio::io_context& io_context) : socket_(io_context) {}
 
-    auto set_index(size_t index) -> void;
-    auto get_index() const -> size_t;
-
-    auto on_accept() -> void;
-    auto reset() -> void;
-
-    auto read_from_socket() -> std::string;
-
-public:
-    auto get_socket() -> tcp_t::socket&;
+	auto set_index(size_t index) -> void;
+	auto get_index() const -> size_t;
+	auto on_accept() -> void;
+	auto reset() -> void;
+	auto get_socket() -> tcp_t::socket&;
 
 private:
-    size_t index_;
+	auto read_from_socket() -> void;
 
-    std::array<char, message_size> buf_{};
-    tcp_t::socket socket_;
+private:
+	tcp_t::socket socket_;
+	size_t index_{ 0 };
+
+	// 데이터 수신 버퍼
+	static constexpr std::size_t BUFFER_SIZE = 8192;
+	std::array<char, BUFFER_SIZE> buf_{};
 };
